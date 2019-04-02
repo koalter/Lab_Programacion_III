4eaf2ac0d9b89d2ef18a405d2e5326bb2e695d4b
koalter <heavens_drive090@hotmail.com>
2019-04-01 21:55:43 -0300

    Imagenes

---
+++ Clase03/Clases/Alumno.php
@@ -0,0 +1,12 @@
+<?php
+
+class Alumno {
+    public $apellido;
+    public $legajo;
+
+    function __construct($apellido, $legajo) {
+        $this->apellido = $apellido;
+        $this->legajo = $legajo;
+    }
+}
+?>
---
+++ Clase03/Funciones/GuardarFoto.php
@@ -0,0 +1,28 @@
+<?php
+include_once '../Clases/Alumno.php';
+//var_dump($_FILES);
+
+if ($_FILES != null) {
+    $alumno = new Alumno($_POST['apellido'], $_POST['legajo']);
+    $origen = $_FILES['foto']['tmp_name'];
+    //TODO: hacer un chequeo de la extension para asegurarse que solo sea jpg o png
+    $filename = $alumno->legajo.$alumno->apellido;
+    $destino = '../Fotos/'.$filename.'.'.pathinfo($_FILES['foto']['name'], PATHINFO_EXTENSION);
+
+    if (file_exists($destino)) {
+        //codigo para realizar el backup
+        $backupPath = '../FotosBackup/'.$filename.date('(dmy)', time()).'.'.pathinfo($_FILES['foto']['name'], PATHINFO_EXTENSION);
+        $backup = rename($destino, $backupPath);
+        
+        if ($backup) {
+            echo "<p>Se hizo un backup del archivo</p>";
+        }
+    }
+    $return = move_uploaded_file($origen, $destino);
+
+    if ($return) {
+        echo "<p>El archivo se movio a Fotos</p>";
+    }
+}
+
+?>
